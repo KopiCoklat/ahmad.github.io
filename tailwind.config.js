93105fc650e9773a15d2eb3cf2720610cbde1b0f
KopiCoklat <sendia872@gmail.com>
2023-11-11 03:50:50 +0000

    v1

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content:['index.htm'],
+  theme: {
+    container:{
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors:{
+        primary: '#06b6d4',
+        secondary: '#64748b',
+      }
+    },
+  },
+  plugins: [],
+}
